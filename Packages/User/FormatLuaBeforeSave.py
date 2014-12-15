56ca9550a11d313bfae4c3090be7f530f3981c5d
unknown <hanxl@UPC0743.uz.local>
2014-12-15 17:12:37 +0800

    x

--- Packages/User/FormatLuaBeforeSave.py
+++ Packages/User/FormatLuaBeforeSave.py
@@ -2,5 +2,6 @@ import sublime, sublime_plugin, re
 
 class FormatLuaBeforeSave(sublime_plugin.EventListener):
     def on_pre_save(self, view):
+        pass
         # if re.search("lua$",view.file_name()):
         #     view.run_command("format_lua")
